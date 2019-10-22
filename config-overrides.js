c1689ad8d8e63af51e09bf80a9d6c640d32c7675
ihsan <ihksansanhas@gmail.com>
2019-10-23 05:03:48 +0700

    watch all with devserver

--- config-overrides.js
+++ config-overrides.js
@@ -2,22 +2,31 @@ const {
   override,
   addDecoratorsLegacy,
   addWebpackAlias,
-  addBundleVisualizer
+  addBundleVisualizer,
+  overrideDevServer,
+  watchAll
 } = require("customize-cra");
 const path = require("path");
 
-module.exports = override(
-  // enable legacy decorators babel plugin
-  addDecoratorsLegacy(),
+module.exports = {
+  webpack: override(
+    // enable legacy decorators babel plugin
+    addDecoratorsLegacy(),
 
-  // add webpack bundle visualizer if BUNDLE_VISUALIZE flag is enabled
-  process.env.BUNDLE_VISUALIZE == 1 && addBundleVisualizer(),
+    // add webpack bundle visualizer if BUNDLE_VISUALIZE flag is enabled
+    process.env.BUNDLE_VISUALIZE == 1 && addBundleVisualizer(),
 
-  // add an alias for "ag-grid-react" imports
-  addWebpackAlias({
-    Pages: path.resolve(__dirname, "src/containers/Pages"),
-    Templates: path.resolve(__dirname, "src/containers/Templates"),
-    Utils: path.resolve(__dirname, "src/utils"),
-    Components: path.resolve(__dirname, "src/components")
-  })
-);
+    // add an alias for "ag-grid-react" imports
+    addWebpackAlias({
+      Pages: path.resolve(__dirname, "src/containers/Pages"),
+      Templates: path.resolve(__dirname, "src/containers/Templates"),
+      Utils: path.resolve(__dirname, "src/utils"),
+      Components: path.resolve(__dirname, "src/components")
+    })
+  ),
+  // dev server
+  devServer: overrideDevServer(
+    // dev server plugin
+    watchAll()
+  )
+};
